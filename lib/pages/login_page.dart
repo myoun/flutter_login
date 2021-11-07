a717e2a18d47b4a003f8b7d163d43b16963e7f52
myoun <aiden080605@gmail.com>
2021-11-07 15:21:11 +0900

    Flutter Login App

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login/components/custom_form.dart';
+import 'package:flutter_login/components/logo.dart';
+import 'package:flutter_login/size.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          SizedBox(height: xlarge_gap),
+          Logo("Login"),
+          SizedBox(height: large_gap),
+          CustomForm(),
+        ],
+      ),
+    );
+  }
+}
